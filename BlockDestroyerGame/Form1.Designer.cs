1281008e804dd6205467740fb937bd4c5ab28313
khalildaibes <40004665+khalildaibes@users.noreply.github.com>
2019-04-19 14:30:10 +0300

    Add project files.

---
+++ BlockDestroyerGame/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BlockDestroyerGame
+{
+    partial class BlockDestroyerGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BallMovement = new System.ComponentModel.BackgroundWorker();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Stick = new System.Windows.Forms.PictureBox();
+            this.CreateBlocks = new System.ComponentModel.BackgroundWorker();
+            this.TimeGame = new System.Windows.Forms.Timer(this.components);
+            this.timelbl = new System.Windows.Forms.Label();
+            this.Lifelbl = new System.Windows.Forms.Label();
+            this.LifeTimer = new System.Windows.Forms.Timer(this.components);
+            this.Winloselbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BallMovement
+            // 
+            this.BallMovement.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BallMovement_DoWork);
+            this.BallMovement.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BallMovement_ProgressChanged);
+            // 
+            // Ball
+            // 
+            this.Ball.Image = global::BlockDestroyerGame.Properties.Resources._1421058796390;
+            this.Ball.Location = new System.Drawing.Point(307, 414);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 1;
+            this.Ball.TabStop = false;
+            // 
+            // Stick
+            // 
+            this.Stick.Image = global::BlockDestroyerGame.Properties.Resources._1421058561125;
+            this.Stick.Location = new System.Drawing.Point(260, 435);
+            this.Stick.Name = "Stick";
+            this.Stick.Size = new System.Drawing.Size(120, 15);
+            this.Stick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Stick.TabIndex = 0;
+            this.Stick.TabStop = false;
+
+            // 
+            // CreateBlocks
+            // 
+            this.CreateBlocks.DoWork += new System.ComponentModel.DoWorkEventHandler(this.CreateBlocks_DoWork);
+            this.CreateBlocks.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.CreateBlocks_ProgressChanged);
+            // 
+            // TimeGame
+            // 
+            this.TimeGame.Interval = 1000;
+            this.TimeGame.Tick += new System.EventHandler(this.TimeGame_Tick);
+            // 
+            // timelbl
+            // 
+            this.timelbl.BackColor = System.Drawing.Color.Black;
+            this.timelbl.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.timelbl.ForeColor = System.Drawing.Color.White;
+            this.timelbl.Location = new System.Drawing.Point(0, 0);
+            this.timelbl.Name = "timelbl";
+            this.timelbl.Size = new System.Drawing.Size(115, 20);
+            this.timelbl.TabIndex = 2;
+
+            // 
+            // Lifelbl
+            // 
+            this.Lifelbl.BackColor = System.Drawing.Color.Black;
+            this.Lifelbl.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Lifelbl.ForeColor = System.Drawing.Color.White;
+            this.Lifelbl.Location = new System.Drawing.Point(568, 0);
+            this.Lifelbl.Name = "Lifelbl";
+            this.Lifelbl.Size = new System.Drawing.Size(115, 20);
+            this.Lifelbl.TabIndex = 3;
+            // 
+            // LifeTimer
+            // 
+            this.LifeTimer.Enabled = true;
+            this.LifeTimer.Interval = 1;
+            this.LifeTimer.Tick += new System.EventHandler(this.LifeTimer_Tick);
+            // 
+            // Winloselbl
+            // 
+            this.Winloselbl.BackColor = System.Drawing.Color.Transparent;
+            this.Winloselbl.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Winloselbl.ForeColor = System.Drawing.Color.White;
+            this.Winloselbl.Location = new System.Drawing.Point(199, 175);
+            this.Winloselbl.Name = "Winloselbl";
+            this.Winloselbl.Size = new System.Drawing.Size(300, 95);
+            this.Winloselbl.TabIndex = 4;
+            this.Winloselbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Winloselbl.Visible = false;
+            // 
+            // BlockDestroyerGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 466);
+            this.Controls.Add(this.Winloselbl);
+            this.Controls.Add(this.Lifelbl);
+            this.Controls.Add(this.timelbl);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Stick);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(700, 500);
+            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Name = "BlockDestroyerGame";
+            this.Text = "Block Destroyer Game";
+            this.Load += new System.EventHandler(this.BlockDestroyerGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BlockDestroyerGame_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stick)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker BallMovement;
+        private System.Windows.Forms.PictureBox Stick;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.ComponentModel.BackgroundWorker CreateBlocks;
+        private System.Windows.Forms.Timer TimeGame;
+        private System.Windows.Forms.Label timelbl;
+        private System.Windows.Forms.Label Lifelbl;
+        private System.Windows.Forms.Timer LifeTimer;
+        private System.Windows.Forms.Label Winloselbl;
+    }
+}
+
---
+++ BlockDestroyerGame/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace BlockDestroyerGame
+{
+    public partial class BlockDestroyerGame : Form
+    {
+        public void CheckBall()
+        {
+            if (this.x > this.Width - 50)
+                this.gx = -10;
+            else if (this.x < 0)
+                this.gx = 10;
+            if (Ball.Bounds.IntersectsWith(Stick.Bounds))
+            {
+                if (this.x + 20 >= Stick.Location.X && this.x < Stick.Location.X + 60)
+                    this.gx = -10;
+
+                else if (this.x > Stick.Location.X + 60 && this.x <= Stick.Location.X + 120)
+                    this.gx = 10;
+                this.gy = -10;
+            }
+            else if (this.y < 0)
+                this.gy = 10;
+            
+        }
+        public void CheckBoom(Label b,int i)
+        {
+            if (b.Bounds.IntersectsWith(Ball.Bounds) && b.Visible == true)
+            {
+                score++;
+                //Location X
+                if (Ball.Location.X + 20 >= b.Location.X && Ball.Location.X <= b.Location.X + 45 || Ball.Location.X > b.Location.X + 45 && Ball.Location.X <= b.Location.X + 90)
+                {
+                    if (this.boom[i] == 2)
+                    {
+                        b.BackColor = Color.Green;
+                        this.boom[i]--;
+                    }
+                    else
+                    {
+                        b.Visible = false;
+                        this.boom[i]--;
+                    }
+                    this.gy *= -1;
+                }
+               
+                //Location Y
+                else if (Ball.Location.Y + 20 >= b.Location.Y && Ball.Location.Y <= b.Location.Y + 15 || Ball.Location.Y > b.Location.Y + 15 && Ball.Location.Y <= b.Location.Y + 25)
+                {
+                    if (this.boom[i] == 2)
+                    {
+                        b.BackColor = Color.Green;
+                        this.boom[i]--;
+                    }
+                    else
+                    {
+                        b.Visible = false;
+                        this.boom[i]--;
+                    }
+                    this.gx *= -1;
+                }
+            }
+        }
+        public BlockDestroyerGame()
+        {
+            InitializeComponent();
+        }
+        private Label[] blocks;
+        private int gx=10, gy=10, x, y, xb = 34, yb = 34, t=0, m=0, l=3, score = 0;
+        private int[] boom = new int[24];
+        private bool flag = false;
+        private void BallMovement_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (flag)
+            {
+                BallMovement.ReportProgress(1);
+                Thread.Sleep(35);
+            }
+        }
+        private void BallMovement_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            CheckBall();
+            for (int i = 0; i < 24; i++)
+                CheckBoom(blocks[i], i);
+            Point p1;
+            x += gx;
+            y += gy;
+            p1 = new Point(x, y);
+            Ball.Location = p1;
+        }
+        private void BlockDestroyerGame_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                if (i == 8 || i == 9 || i == 14 || i == 15)
+                    boom[i] = 1;
+                else
+                boom[i] = 2;
+            }            
+            CreateBlocks.WorkerReportsProgress = true;
+            CreateBlocks.RunWorkerAsync();
+        }
+        private void BlockDestroyerGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if (this.score != 44 && l > 0)
+                    {
+                        if (Stick.Location.X + 120 < this.Width - 50)
+                        {
+                            Stick.Left += 50;
+                            if (!flag)
+                                Ball.Left += 50;
+                        }
+                    }
+                    break;
+                case Keys.Left:
+                    if (this.score != 44 && l > 0)
+                    {
+                        if (Stick.Location.X > 50)
+                        {
+                            Stick.Left -= 50;
+                            if (!flag)
+                                Ball.Left -= 50;
+                        }
+                    }
+                    break;
+                case Keys.Space:
+                    if (this.score != 44 && l > 0)
+                    {
+                        this.flag = true;
+                        if (!BallMovement.IsBusy)
+                        {
+                            BallMovement.WorkerReportsProgress = true;
+                            BallMovement.RunWorkerAsync();
+                            this.x = Stick.Location.X + 47;
+                            this.y = Stick.Location.Y - 15;
+                        }
+                        TimeGame.Enabled = true;
+                        LifeTimer.Enabled = true;
+                    }
+                                 break;
+            }
+        }
+        private void CreateBlocks_DoWork(object sender, DoWorkEventArgs e)
+        {
+            blocks = new Label[24];
+            for (int i = 0; i < 24; i++)
+            {
+                CreateBlocks.ReportProgress(i);
+                Thread.Sleep(20);
+            }
+        }
+        private void CreateBlocks_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if ((yb == 71 && xb == 246) || (yb == 71 && xb == 352) || (yb == 108 && xb == 246) || (yb == 108 && xb == 352))
+            {
+                blocks[e.ProgressPercentage] = new Label();
+                blocks[e.ProgressPercentage].AutoSize = false;
+                blocks[e.ProgressPercentage].Size = new System.Drawing.Size(90, 25);
+                blocks[e.ProgressPercentage].BackColor = Color.Green;
+                blocks[e.ProgressPercentage].Location = new Point(xb, yb);
+                this.Controls.Add(blocks[e.ProgressPercentage]);
+            }
+            else
+            {
+                blocks[e.ProgressPercentage] = new Label();
+                blocks[e.ProgressPercentage].AutoSize = false;
+                blocks[e.ProgressPercentage].Size = new System.Drawing.Size(90, 25);
+                blocks[e.ProgressPercentage].BackColor = Color.Red;
+                blocks[e.ProgressPercentage].Location = new Point(xb, yb);
+                this.Controls.Add(blocks[e.ProgressPercentage]);
+            }
+            xb += 106;
+            
+            if (xb == 670)
+            {
+                xb = 34;
+                yb += 37;
+            }
+            
+        }
+        private void TimeGame_Tick(object sender, EventArgs e)
+        {
+            if (t == 60)
+            {
+                m++;
+                t=0;
+            }
+            t++;
+            timelbl.Text = "Time: 0" + m + ":" + t;           
+        }
+        private void LifeTimer_Tick(object sender, EventArgs e)
+        {
+            Lifelbl.Text = "Life: " + l;
+            if (this.score == 44)
+            {
+                Winloselbl.BackColor = Color.Green;
+                Winloselbl.Text = "YOU WIN";
+                Winloselbl.Visible = true;
+                this.flag = false;
+                TimeGame.Enabled = false;
+                LifeTimer.Enabled = false;
+            }
+            if (Ball.Location.Y > this.Height)
+            {
+                    this.flag = false;
+                    Ball.Location = new Point(Stick.Location.X + 47, Stick.Location.Y - 20);
+                    l--;
+                    if (this.l == 0)
+                    {
+                        Winloselbl.Text = "GAME OVER, Your Score Is:" + this.score + "";
+                        Winloselbl.BackColor = Color.Red;
+                        Winloselbl.Visible = true;
+                    }
+                    Lifelbl.Text = "Life: " + l;
+                    TimeGame.Enabled = false;
+                    LifeTimer.Enabled = false;
+                
+            }
+        }
+
+
+    }
+}
